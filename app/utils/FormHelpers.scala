101ee3f4c9722ef989a12c1bb83fbed61095e7da
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2018-08-20 14:32:53 +0100

    Initial commit

---
+++ app/utils/FormHelpers.scala
@@ -0,0 +1,18 @@
+package utils
+
+import play.api.data.Form
+import play.api.i18n.Messages
+
+object FormHelpers {
+
+  def getErrorByKey[A](form: Form[_], errorKey: String) = {
+    form.error(errorKey) match {
+      case None => ""
+      case Some(error) => error.message
+    }
+  }
+
+  def errorPrefix(form: Form[_])(implicit messages: Messages): String = {
+    if (form.hasErrors || form.hasGlobalErrors) messages("error.browser.title.prefix") else ""
+  }
+}
