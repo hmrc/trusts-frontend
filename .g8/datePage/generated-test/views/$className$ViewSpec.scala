0982f8fb5f497a45761f9a6136a4fc95052465ae
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-12-07 10:35:51 +0000

    TRUS-463 | added datepage scaffold from IPR

---
+++ .g8/datePage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,33 @@
+package views
+
+import java.time.LocalDate
+
+import forms.$className$FormProvider
+import models.{AfaId, NormalMode, UserAnswers}
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.QuestionViewBehaviours
+import views.html.$className$View
+
+class $className$ViewSpec extends QuestionViewBehaviours[LocalDate] {
+
+  val messageKeyPrefix = "$className;format="decap"$"
+
+  val afaId = AfaId("1")
+
+  val form = new $className$FormProvider()()
+
+  "$className$View view" must {
+
+    val application = applicationBuilder(userAnswers = Some(UserAnswers(afaId.value))).build()
+
+    val view = application.injector.instanceOf[$className$View]
+
+    def applyView(form: Form[_]): HtmlFormat.Appendable =
+      view.apply(form, NormalMode, afaId)(fakeRequest, messages)
+
+    behave like normalPage(applyView(form), messageKeyPrefix)
+
+    behave like pageWithBackLink(applyView(form))
+  }
+}
