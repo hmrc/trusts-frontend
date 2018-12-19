7c4f74419bfa6acfeefb515cf28d28f14fa9a52f
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-12-19 11:02:48 +0000

    TRUS-463 | Updated content to match scots law page to the spreadsheet

--- test/views/EstablishedUnderScotsLawViewSpec.scala
+++ test/views/EstablishedUnderScotsLawViewSpec.scala
@@ -44,5 +44,7 @@ class EstablishedUnderScotsLawViewSpec extends YesNoViewBehaviours {
     behave like pageWithBackLink(applyView(form))
 
     behave like yesNoPage(form, applyView, messageKeyPrefix, routes.EstablishedUnderScotsLawController.onSubmit(NormalMode).url)
+
+    behave like pageWithASubmitButton(applyView(form))
   }
 }
