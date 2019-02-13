368bd74a423e39b0b212e06af3b6be9ef84f59c4
Adam Conder <adamdconder@gmail.com>
2019-02-13 16:52:04 +0000

    TRUS-707 Implement OptionsBehaviour

--- test/views/TrusteeOrIndividualViewSpec.scala
+++ test/views/TrusteeOrIndividualViewSpec.scala
@@ -20,10 +20,10 @@ import forms.TrusteeOrIndividualFormProvider
 import models.{NormalMode, TrusteeOrIndividual}
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
-import views.behaviours.ViewBehaviours
+import views.behaviours.OptionsViewBehaviours
 import views.html.TrusteeOrIndividualView
 
-class TrusteeOrIndividualViewSpec extends ViewBehaviours {
+class TrusteeOrIndividualViewSpec extends OptionsViewBehaviours {
 
   val messageKeyPrefix = "trusteeOrIndividual"
 
@@ -43,37 +43,8 @@ class TrusteeOrIndividualViewSpec extends ViewBehaviours {
     behave like normalPage(applyView(form), messageKeyPrefix)
 
     behave like pageWithBackLink(applyView(form))
+
+    behave like pageWithOptions(form, applyView, TrusteeOrIndividual.options)
   }
-
-  "TrusteeOrIndividualView" when {
-
-    "rendered" must {
-
-      "contain radio buttons for the value" in {
-
-        val doc = asDocument(applyView(form))
-
-        for (option <- TrusteeOrIndividual.options) {
-          assertContainsRadioButton(doc, option.id, "value", option.value, false)
-        }
-      }
-    }
-
-    for (option <- TrusteeOrIndividual.options) {
-
-      s"rendered with a value of '${option.value}'" must {
-
-        s"have the '${option.value}' radio button selected" in {
-
-          val doc = asDocument(applyView(form.bind(Map("value" -> s"${option.value}"))))
-
-          assertContainsRadioButton(doc, option.id, "value", option.value, true)
-
-          for (unselectedOption <- TrusteeOrIndividual.options.filterNot(o => o == option)) {
-            assertContainsRadioButton(doc, unselectedOption.id, "value", unselectedOption.value, false)
-          }
-        }
-      }
-    }
-  }
+  
 }
