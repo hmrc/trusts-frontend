0a36fb720054370cd1f8053dbe6f51e10fe3e78e
Adam Conder <adamdconder@gmail.com>
2019-03-13 15:10:03 +0000

    TRUS-744 Scaffold task list page

---
+++ migrations/applied_migrations/TaskList.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration TaskList"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        /taskList                       controllers.TaskListController.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "taskList.title = taskList" >> ../conf/messages.en
+echo "taskList.heading = taskList" >> ../conf/messages.en
+
+echo "Migration TaskList completed"
