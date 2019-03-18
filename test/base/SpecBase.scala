8cfa31bfdc8cca66526be51a19b8a39750ceaa67
Adam Conder <adamdconder@gmail.com>
2019-03-18 12:31:03 +0000

    TRUS-744 Change instance

--- test/base/SpecBase.scala
+++ test/base/SpecBase.scala
@@ -56,6 +56,6 @@ trait SpecBase extends PlaySpec with GuiceOneAppPerSuite with TryValues with Moc
         bind[DataRequiredAction].to[DataRequiredActionImpl],
         bind[IdentifierAction].toInstance(new FakeIdentifierAction(affinityGroup)(injectedParsers)),
         bind[DataRetrievalAction].toInstance(new FakeDataRetrievalAction(userAnswers)),
-        bind[SessionRepository].to(mockedSessionRepository)
+        bind[SessionRepository].toInstance(mockedSessionRepository)
       )
 }
