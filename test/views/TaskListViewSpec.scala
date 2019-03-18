41c4ecf46309dac4d7242fffc72ee389b350909a
Adam Conder <adamdconder@gmail.com>
2019-03-18 09:43:09 +0000

    TRUS-744 Add test for asserting agent registrations link

--- test/views/TaskListViewSpec.scala
+++ test/views/TaskListViewSpec.scala
@@ -29,27 +29,50 @@ class TaskListViewSpec extends ViewBehaviours with TaskListViewBehaviours {
   private val dateFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy")
   private val savedUntil : String = LocalDateTime.now.format(dateFormatter)
 
-  "TaskList view" must {
+  "TaskList view" when {
 
-    val expectedSections = List(
-      Task("trust-details", routes.AddATrusteeController.onPageLoad(), Some(Completed)),
-      Task("settlors", routes.AddATrusteeController.onPageLoad(), Some(InProgress)),
-      Task("trustees", routes.AddATrusteeController.onPageLoad(), Some(InProgress)),
-      Task("beneficiaries", routes.AddATrusteeController.onPageLoad(), None),
-      Task("assets", routes.AddATrusteeController.onPageLoad(), None),
-      Task("tax-liability", routes.AddATrusteeController.onPageLoad(), Some(Completed))
-    )
+    "rendered for an Organisation or an Agent" must {
+        val expectedSections = List(
+          Task("trust-details", routes.AddATrusteeController.onPageLoad(), Some(Completed)),
+          Task("settlors", routes.AddATrusteeController.onPageLoad(), Some(InProgress)),
+          Task("trustees", routes.AddATrusteeController.onPageLoad(), Some(InProgress)),
+          Task("beneficiaries", routes.AddATrusteeController.onPageLoad(), None),
+          Task("assets", routes.AddATrusteeController.onPageLoad(), None),
+          Task("tax-liability", routes.AddATrusteeController.onPageLoad(), Some(Completed))
+        )
 
-    val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
-    val view = application.injector.instanceOf[TaskListView]
+        val view = application.injector.instanceOf[TaskListView]
 
-    val applyView = view.apply(savedUntil, expectedSections)(fakeRequest, messages)
+        val applyView = view.apply(savedUntil, expectedSections)(fakeRequest, messages)
 
-    behave like normalPage(applyView, "taskList")
+        behave like normalPage(applyView, "taskList")
 
-    behave like pageWithBackLink(applyView)
+        behave like pageWithBackLink(applyView)
 
-    behave like taskList(applyView, expectedSections)
+        behave like taskList(applyView, expectedSections)
+      }
+    }
+
+  "rendered for an Agent" must {
+
+    "render return to saved registrations link" in {
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      val view = application.injector.instanceOf[TaskListView]
+
+      val applyView = view.apply(savedUntil, Nil)(fakeRequest, messages)
+
+      val doc = asDocument(applyView)
+
+      assertAttributeValueForElement(
+        doc.getElementById("saved-registrations"),
+        "href",
+        ""
+      )
+    }
+
   }
+
 }
