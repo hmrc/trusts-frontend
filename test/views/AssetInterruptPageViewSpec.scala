1de8a4eac208f2269b188b45b661a2d275edc5b0
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2019-03-19 13:13:30 +0000

    TRUS-811 | Fixed failing tests

--- test/views/AssetInterruptPageViewSpec.scala
+++ test/views/AssetInterruptPageViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views
 
 import views.behaviours.ViewBehaviours
@@ -13,8 +29,29 @@ class AssetInterruptPageViewSpec extends ViewBehaviours {
 
     val applyView = view.apply()(fakeRequest, messages)
 
-    behave like normalPage(applyView, "assetInterruptPage")
+    behave like normalPage(applyView, "assetInterruptPage", "title",
+      "subheading1",
+      "paragraph1",
+      "subheading2",
+      "paragraph2",
+      "bullet1",
+      "bullet2",
+      "bullet3",
+      "subheading3",
+      "paragraph3",
+      "bullet4",
+      "bullet5",
+      "bullet6",
+      "paragraph4",
+      "subheading4",
+      "paragraph5",
+      "bullet7",
+      "bullet8",
+      "bullet9",
+      "paragraph6",
+      "continue")
 
     behave like pageWithBackLink(applyView)
+
   }
 }
