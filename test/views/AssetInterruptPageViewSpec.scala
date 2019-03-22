4abca89fb775523823b909df5d097de5920b77bb
Joshua Aaron Hill <34240122+JoshuaH01@users.noreply.github.com>
2019-03-22 10:21:53 +0000

    Trus 811- Created AssetInteruptPage | Added tests and CSS to fix padding (#53)
    * TRUS-811 | Created interuptpage
    
    * TRUS-811 | Added content to page
    
    * TRUS-811 | Fixed failing tests
    
    * TRUS-811 | Added additional css to fix padding
    
    * TRUS-811 | Added applictation stop to view tests
    
    * TRUS-811 | made pr changes
    
    * TRUS-811 | PR changes made. Edited messages file and the link
    
    * TRUS-812 | edited app routes file as used Assets in the url which caused the get to brake as it went looking in to the assets file under templates
    
    * Update messages.en

---
+++ test/views/AssetInterruptPageViewSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import views.behaviours.ViewBehaviours
+import views.html.AssetInterruptPageView
+
+class AssetInterruptPageViewSpec extends ViewBehaviours {
+
+  "AssetInterruptPage view" must {
+
+    val view = viewFor[AssetInterruptPageView](Some(emptyUserAnswers))
+
+    val applyView = view.apply()(fakeRequest, messages)
+
+    behave like normalPage(applyView, "assetInterruptPage", "title",
+      "subheading1",
+      "paragraph1",
+      "subheading2",
+      "paragraph2",
+      "bullet1",
+      "bullet2",
+      "bullet3",
+      "subheading3",
+      "paragraph3",
+      "bullet4",
+      "bullet5",
+      "bullet6",
+      "paragraph4",
+      "subheading4",
+      "paragraph5",
+      "bullet7",
+      "bullet8",
+      "bullet9",
+      "paragraph6")
+
+
+    behave like pageWithBackLink(applyView)
+
+  }
+}
