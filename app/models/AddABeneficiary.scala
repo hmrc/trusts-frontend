ca3d26a219b099dc2161473e2f685908f7fb1091
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2019-04-24 15:49:48 +0100

    modified add another

--- app/models/AddABeneficiary.scala
+++ app/models/AddABeneficiary.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models
 
 import play.api.libs.json._
@@ -7,11 +23,11 @@ sealed trait AddABeneficiary
 
 object AddABeneficiary extends Enumerable.Implicits {
 
-  case object 1 extends WithName("1") with AddABeneficiary
-  case object 2 extends WithName("2") with AddABeneficiary
+  case object One extends WithName("1") with AddABeneficiary
+  case object Two extends WithName("2") with AddABeneficiary
 
   val values: List[AddABeneficiary] = List(
-    1, 2
+    One, Two
   )
 
   val options: List[RadioOption] = values.map {
