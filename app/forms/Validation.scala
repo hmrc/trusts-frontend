0ce8c62e8a33df4b7ac2ae66709a9de433fec835
Sachin Gupta <codebysachin@gmail.com>
2019-04-30 13:42:31 +0100

    added validations and hint text for incomeyesno and income page

--- app/forms/Validation.scala
+++ app/forms/Validation.scala
@@ -28,6 +28,7 @@ object Validation {
   val addressLineRegex = "^[A-Za-z0-9 ,.()/&'-]*$"
   val clientRefRegex = "^[A-Za-z0-9&'`-]*$"
   val currencyRegex = "^[0-9]{1,12}$"
+  val numericRegex = "^[0-9]*$"
   val decimalCheck = "^[^.]*$"
 
 }
