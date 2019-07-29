93ab8e04098ccd97a7414752fb43f3b8783ba274
thomascurry <thomasjcurry@hotmail.com>
2019-07-29 16:15:26 +0100

    Move share asset tests into individual mapper

--- test/mapping/MoneyAssetMapperSpec.scala
+++ test/mapping/MoneyAssetMapperSpec.scala
@@ -18,11 +18,37 @@ package mapping
 
 import base.SpecBaseHelpers
 import generators.Generators
+import models.Status.Completed
+import models.WhatKindOfAsset
 import org.scalatest.{FreeSpec, MustMatchers, OptionValues}
+import pages.entitystatus.AssetStatus
+import pages.{AssetMoneyValuePage, WhatKindOfAssetPage}
 
 class MoneyAssetMapperSpec extends FreeSpec with MustMatchers
   with OptionValues with Generators with SpecBaseHelpers {
 
-//  val moneyAssetMapper : Mapping[AssetMonetaryAmount] = injector.instanceOf[MoneyAssetMapper]
+  val moneyAssetMapper : Mapping[List[AssetMonetaryAmount]] = injector.instanceOf[MoneyAssetMapper]
 
+  "MoneyAssetMapper" - {
+
+    "must not be able to create a money asset when no value is in user answers" in {
+
+      val userAnswers =
+        emptyUserAnswers
+          .set(WhatKindOfAssetPage(0), WhatKindOfAsset.Money).success.value
+
+      moneyAssetMapper.build(userAnswers) mustNot be(defined)
+    }
+
+    "must able to create a Monetary Asset" in {
+
+      val userAnswers =
+        emptyUserAnswers
+          .set(WhatKindOfAssetPage(0), WhatKindOfAsset.Money).success.value
+          .set(AssetMoneyValuePage(0), "2000").success.value
+          .set(AssetStatus(0), Completed).success.value
+
+      moneyAssetMapper.build(userAnswers).value mustBe List(AssetMonetaryAmount(2000))
+    }
+  }
 }
