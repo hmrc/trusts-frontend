c3e147b9a0812ee477dcff1aa8acd8117d5c89a2
Adam Conder <adamdconder@gmail.com>
2019-08-02 11:05:05 +0100

    Add beneficiary view helper implemented with message files and defaults

--- app/utils/AddABeneficiaryViewHelper.scala
+++ app/utils/AddABeneficiaryViewHelper.scala
@@ -25,25 +25,24 @@ import viewmodels.{AddRow, AddToRows}
 class AddABeneficiaryViewHelper(userAnswers: UserAnswers)(implicit messages: Messages) {
 
   private def parseName(name : Option[FullName]) : String = {
-    name match {
-      case Some(x) => s"$x"
-      case None => ""
-    }
+    val defaultValue = messages("entities.no.name.added")
+    name.map(_.toString).getOrElse(defaultValue)
   }
 
   private def parseIndividualBeneficiary(individualBeneficiary : IndividualBeneficiaryViewModel) : AddRow = {
     AddRow(
       parseName(individualBeneficiary.name),
-      "Individual Beneficiary",
+      messages("entities.beneficiary.individual"),
       "#",
       "#"
     )
   }
 
   private def parseClassOfBeneficiary(classOfBeneficiary : ClassOfBeneficiaryViewModel) : AddRow = {
+    val defaultValue = messages("entities.no.description.added")
     AddRow(
-      classOfBeneficiary.description.getOrElse(""),
-      "Class of beneficiaries",
+      classOfBeneficiary.description.getOrElse(defaultValue),
+      messages("entities.beneficiary.class"),
       "#",
       "#"
     )
@@ -62,8 +61,10 @@ class AddABeneficiaryViewHelper(userAnswers: UserAnswers)(implicit messages: Mes
 
     val classOfBeneficiariesInProgress = classOfBeneficiaries.filterNot(_.isComplete).map(parseClassOfBeneficiary)
 
-    AddToRows(indBenInProgress ::: classOfBeneficiariesInProgress,
-      indBeneficiaryComplete ::: classOfBeneficiariesComplete)
+    AddToRows(
+      inProgress = indBenInProgress ::: classOfBeneficiariesInProgress,
+      complete = indBeneficiaryComplete ::: classOfBeneficiariesComplete
+    )
   }
 
 }
