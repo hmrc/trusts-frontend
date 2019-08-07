25fb365ad4f6081f0c2cb1fb0454658e2edb891b
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2019-08-07 08:38:36 +0100

    initial commit

---
+++ app/controllers/property_or_land/PropertyOrLandAnswerController.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.property_or_land
+
+import controllers.actions._
+import controllers.filters.IndexActionFilterProvider
+import javax.inject.Inject
+import models.NormalMode
+import models.Status.Completed
+import navigation.Navigator
+import pages.entitystatus.AssetStatus
+import pages.shares.{ShareAnswerPage, SharesInAPortfolioPage}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.CheckYourAnswersHelper
+import utils.countryOptions.CountryOptions
+import viewmodels.AnswerSection
+import views.html.property_or_land.PropertyOrLandAnswersView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class PropertyOrLandAnswerController @Inject()(
+                                      override val messagesApi: MessagesApi,
+                                      sessionRepository: SessionRepository,
+                                      navigator: Navigator,
+                                      identify: IdentifierAction,
+                                      getData: DraftIdRetrievalActionProvider,
+                                      requireData: DataRequiredAction,
+                                      requiredAnswer: RequiredAnswerActionProvider,
+                                      validateIndex: IndexActionFilterProvider,
+                                      view: PropertyOrLandAnswersView,
+                                      countryOptions: CountryOptions,
+                                      val controllerComponents: MessagesControllerComponents
+                                     )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private def actions(index: Int, draftId: String) =
+    identify andThen
+      getData(draftId) andThen
+      requireData // andThen
+//      requiredAnswer(RequiredAnswer(PropertyOrLandAddressPage(index), routes.PropertyOrLandAddressController.onPageLoad(NormalMode, index, draftId)))
+
+
+  def onPageLoad(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId) {
+    implicit request =>
+
+      val answers = new CheckYourAnswersHelper(countryOptions)(request.userAnswers, draftId)
+
+      val sections = Seq(
+        AnswerSection(
+          None,
+          request.userAnswers.get(SharesInAPortfolioPage(index)) match {
+            case Some(false) =>
+              Seq(
+                answers.whatKindOfAsset(index),
+                answers.sharesInAPortfolio(index),
+                answers.shareCompanyName(index),
+                answers.sharesOnStockExchange(index),
+                answers.shareClass(index),
+                answers.shareQuantityInTrust(index),
+                answers.shareValueInTrust(index)
+              ).flatten
+            case Some(true) =>
+              Seq(
+                answers.whatKindOfAsset(index),
+                answers.sharesInAPortfolio(index),
+                answers.sharePortfolioName(index),
+                answers.sharePortfolioOnStockExchange(index),
+                answers.sharePortfolioQuantityInTrust(index),
+                answers.sharePortfolioValueInTrust(index)
+              ).flatten
+            case None =>
+              Nil
+          }
+        )
+      )
+
+      Ok(view(index, draftId, sections))
+  }
+
+  def onSubmit(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId).async {
+    implicit request =>
+
+      val answers = request.userAnswers.set(AssetStatus(index), Completed)
+
+      for {
+        updatedAnswers <- Future.fromTry(answers)
+        _ <- sessionRepository.set(updatedAnswers)
+      } yield Redirect(navigator.nextPage(ShareAnswerPage, NormalMode, draftId)(request.userAnswers))
+
+  }
+}
