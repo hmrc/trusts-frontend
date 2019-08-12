b21f3ebdd0f07907fa33b17cde43611e5cfe7442
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2019-08-12 17:57:05 +0100

    [TRUS-1499]- Added Property asset and asset tests

---
+++ app/mapping/reads/PropertyOrLandAsset.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mapping.reads
+
+import models.WhatKindOfAsset.PropertyOrLand
+import models.{Address, PropertyLandValueTrust, WhatKindOfAsset}
+import play.api.libs.json.{JsError, JsSuccess, Reads, __}
+
+final case class PropertyOrLandAsset(override val whatKindOfAsset: WhatKindOfAsset,
+                                     propertyOrLandDescription: Option[String],
+                                     address: Option[Address],
+                                     propertyLandValueTrust: Option[PropertyLandValueTrust],
+                                     propertyOrLandTotalValue: String) extends Asset
+
+object PropertyOrLandAsset {
+
+  import play.api.libs.functional.syntax._
+
+  implicit lazy val reads: Reads[PropertyOrLandAsset] = {
+
+    val landOrPropertyReads: Reads[PropertyOrLandAsset] = (
+      (__ \ "propertyOrLandDescription").readNullable[String] and
+        (__ \ "address").readNullable[Address] and
+          (__ \ "propertyLandValueTrust").readNullable[PropertyLandValueTrust] and
+            (__ \ "propertyOrLandTotalValue").read[String] and
+              (__ \ "whatKindOfAsset").read[WhatKindOfAsset]
+      )((description, address, value, totalValue, kind) => PropertyOrLandAsset(kind, description, address, value, totalValue))
+
+    (__ \ "whatKindOfAsset").read[WhatKindOfAsset].flatMap[WhatKindOfAsset] {
+      whatKindOfAsset: WhatKindOfAsset =>
+        if (whatKindOfAsset == PropertyOrLand) {
+          Reads(_ => JsSuccess(whatKindOfAsset))
+        } else {
+          Reads(_ => JsError("PropertyOrLand asset must be of type `PropertyOrLand`"))
+        }
+    }.andKeep(landOrPropertyReads)
+
+  }
+}
