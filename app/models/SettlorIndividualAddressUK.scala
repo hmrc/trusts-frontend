24a0207d8d6e02e7f9f1c63995287040649b4811
Joshua Aaron Hill <34240122+JoshuaH01@users.noreply.github.com>
2019-08-15 08:48:10 +0100

    Revert "Trus 1520"

--- app/models/SettlorIndividualAddressUK.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class SettlorIndividualAddressUK (field1: String, field2: String)
-
-object SettlorIndividualAddressUK {
-  implicit val format = Json.format[SettlorIndividualAddressUK]
-}
