2e36ffdbf07b2db87a714a453373c1c653041da8
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2019-08-19 15:20:25 +0100

    [TRUS-1118]- Added indexing tests and edited dynamic content

--- test/views/SettlorIndividualDateOfBirthYesNoViewSpec.scala
+++ test/views/SettlorIndividualDateOfBirthYesNoViewSpec.scala
@@ -18,7 +18,7 @@ package views
 
 import controllers.routes
 import forms.SettlorIndividualDateOfBirthYesNoFormProvider
-import models.NormalMode
+import models.{FullName, NormalMode}
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
 import views.behaviours.YesNoViewBehaviours
@@ -28,6 +28,7 @@ class SettlorIndividualDateOfBirthYesNoViewSpec extends YesNoViewBehaviours {
 
   val messageKeyPrefix = "settlorIndividualDateOfBirthYesNo"
   val index = 0
+  val name = FullName("First", Some("middle"), "Last")
 
   val form = new SettlorIndividualDateOfBirthYesNoFormProvider()()
 
@@ -36,12 +37,15 @@ class SettlorIndividualDateOfBirthYesNoViewSpec extends YesNoViewBehaviours {
     val view = viewFor[SettlorIndividualDateOfBirthYesNoView](Some(emptyUserAnswers))
 
     def applyView(form: Form[_]): HtmlFormat.Appendable =
-      view.apply(form, NormalMode, fakeDraftId, index)(fakeRequest, messages)
+      view.apply(form, NormalMode, fakeDraftId, index, name)(fakeRequest, messages)
 
-    behave like normalPage(applyView(form), messageKeyPrefix)
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name.toString)
 
     behave like pageWithBackLink(applyView(form))
 
-    behave like yesNoPage(form, applyView, messageKeyPrefix, routes.SettlorIndividualDateOfBirthYesNoController.onSubmit(NormalMode, index, fakeDraftId).url)
+    behave like yesNoPage(form, applyView, messageKeyPrefix,
+      routes.SettlorIndividualDateOfBirthYesNoController.onSubmit(
+        NormalMode, index, fakeDraftId).url, None, Seq(name.toString))
+
   }
 }
