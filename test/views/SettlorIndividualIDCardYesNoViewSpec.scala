e5157b4bba5681266bcc01212cf1060c3702fe02
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2019-08-20 13:05:52 +0100

    [TRUS-1120]- Added content and index tests

--- test/views/SettlorIndividualIDCardYesNoViewSpec.scala
+++ test/views/SettlorIndividualIDCardYesNoViewSpec.scala
@@ -18,7 +18,7 @@ package views
 
 import controllers.routes
 import forms.SettlorIndividualIDCardYesNoFormProvider
-import models.NormalMode
+import models.{FullName, NormalMode}
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
 import views.behaviours.YesNoViewBehaviours
@@ -28,6 +28,7 @@ class SettlorIndividualIDCardYesNoViewSpec extends YesNoViewBehaviours {
 
   val messageKeyPrefix = "settlorIndividualIDCardYesNo"
   val index = 0
+  val name = FullName("First", Some("middle"), "Last")
 
   val form = new SettlorIndividualIDCardYesNoFormProvider()()
 
@@ -36,13 +37,15 @@ class SettlorIndividualIDCardYesNoViewSpec extends YesNoViewBehaviours {
     val view = viewFor[SettlorIndividualIDCardYesNoView](Some(emptyUserAnswers))
 
     def applyView(form: Form[_]): HtmlFormat.Appendable =
-      view.apply(form, NormalMode, fakeDraftId, index)(fakeRequest, messages)
+      view.apply(form, NormalMode, fakeDraftId, index, name)(fakeRequest, messages)
 
-    behave like normalPage(applyView(form), messageKeyPrefix)
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name.toString)
 
     behave like pageWithBackLink(applyView(form))
 
-    behave like yesNoPage(form, applyView, messageKeyPrefix, routes.SettlorIndividualIDCardYesNoController.onSubmit(NormalMode, index, fakeDraftId).url)
+    behave like yesNoPage(form, applyView, messageKeyPrefix,
+      routes.SettlorIndividualDateOfBirthYesNoController.onSubmit(
+        NormalMode, index, fakeDraftId).url, None, Seq(name.toString))
 
     behave like pageWithASubmitButton(applyView(form))
   }
