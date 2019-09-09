6eefe09637014a5a9d8b32a36680b68268d5f85c
Emilia Trendafilova <emilia.trendafilova@opencastsoftware.com>
2019-09-09 11:53:47 +0100

    TRUS-765: chnaged url to have hyphen

--- app/pages/SettlorsBasedInTheUKPage.scala
+++ app/pages/SettlorsBasedInTheUKPage.scala
@@ -17,10 +17,11 @@
 package pages
 
 import play.api.libs.json.JsPath
+import sections.TrustDetails
 
 case object SettlorsBasedInTheUKPage extends QuestionPage[Boolean] {
 
-  override def path: JsPath = JsPath \ toString
+  override def path: JsPath = JsPath \ TrustDetails \ toString
 
   override def toString: String = "settlorsBasedInTheUK"
 }
