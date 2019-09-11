aaaf527128c1696d329f26562baed01930c4bac4
Emilia Trendafilova <emilia.trendafilova@opencastsoftware.com>
2019-09-11 11:54:38 +0100

    TRUS-554: Fixed test

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -41,6 +41,8 @@ class CheckYourAnswersHelper @Inject()(countryOptions: CountryOptions)(userAnswe
         "settlorBusinessName.checkYourAnswersLabel",
         HtmlFormat.escape(x),
         controllers.living_settlor.routes.SettlorBusinessNameController.onPageLoad(CheckMode, index, draftId).url
+      )
+  }
 
   def settlorKindOfTrust(index: Int): Option[AnswerRow] = userAnswers.get(SettlorKindOfTrustPage) map {
     x =>
