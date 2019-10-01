18e056145671eec7fd9a9634ea973db30f03a62d
Darryl Murphy <darrylmurphy@users.noreply.github.com>
2019-10-01 10:11:12 +0100

    Rename utr variation id

--- app/pages/WhatIsTheUTRVariationPage.scala
+++ app/pages/WhatIsTheUTRVariationPage.scala
@@ -22,5 +22,5 @@ case object WhatIsTheUTRVariationPage extends QuestionPage[String] {
 
   override def path: JsPath = JsPath \ toString
 
-  override def toString: String = "trustUTR"
+  override def toString: String = "whatIsTheUTRVariation"
 }
