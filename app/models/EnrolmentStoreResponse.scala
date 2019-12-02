00f7cb23fb661a6bf1dd686145d649244833d8e2
Adam Conder <adamdconder@gmail.com>
2019-12-02 12:17:54 +0000

    TRUS-1986 Stubbing Enrolment Store ES0 for enrolment lookup
    - Fixing endpoint in enrolment-store-proxy
    - Pointing frontend to enrolment-store-stub port 9595
    - Adding logs to assist monitoring API calls

--- app/models/EnrolmentStoreResponse.scala
+++ app/models/EnrolmentStoreResponse.scala
@@ -39,15 +39,20 @@ object EnrolmentStoreResponse {
   implicit lazy val httpReads: HttpReads[EnrolmentStoreResponse] =
     new HttpReads[EnrolmentStoreResponse] {
       override def read(method: String, url: String, response: HttpResponse): EnrolmentStoreResponse = {
-        Logger.info(s"[AgentTrusts] response status received from ES0 api: ${response.status}, body :${response.body}")
+        Logger.debug(s"[EnrolmentStoreResponse] response status received from ES0 api: ${response.status}, body :${response.body}")
 
         response.status match {
           case OK =>
             response.json.as[EnrolmentStore] match {
-                case EnrolmentStore(Seq(), _) => NotClaimed
-                case _ => AlreadyClaimed
+                case EnrolmentStore(Seq(), _) =>
+                  Logger.info(s"[EnrolmentStoreResponse] UTR has not been claimed")
+                  NotClaimed
+                case _ =>
+                  Logger.info(s"[EnrolmentStoreResponse] UTR has already been claimed")
+                  AlreadyClaimed
             }
           case NO_CONTENT =>
+            Logger.info(s"[EnrolmentStoreResponse] UTR is not claimed or delegated")
             NotClaimed
           case SERVICE_UNAVAILABLE =>
             ServiceUnavailable
@@ -56,6 +61,7 @@ object EnrolmentStoreResponse {
           case BAD_REQUEST =>
             BadRequest
           case _ =>
+            Logger.info(s"[EnrolmentStoreResponse] unexpected response from EnrolmentStore")
             ServerError
         }
       }
