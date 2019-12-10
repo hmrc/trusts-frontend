71de47b2165fc35ac5f191ae6fd2c247f22ba882
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2019-12-10 16:18:07 +0000

    [TRUS-2068] - Un-map Deceased Settlor

---
+++ app/mapping/playback/DeceasedSettlorExtractor.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mapping.playback
+
+import com.google.inject.Inject
+import mapping.playback.PlaybackExtractionErrors.{FailedToExtractData, PlaybackExtractionError}
+import models.core.pages.{FullName, InternationalAddress, UKAddress}
+import models.playback.http.DisplayTrustWillType
+import models.playback.{MetaData, UserAnswers}
+import java.time.LocalDate
+
+import models.registration.pages.PassportOrIdCardDetails
+import pages.register.settlors.deceased_settlor._
+import play.api.Logger
+
+import scala.util.{Failure, Success, Try}
+
+class DeceasedSettlorExtractor @Inject() extends PlaybackExtractor[Option[DisplayTrustWillType]] {
+
+  import PlaybackAddressImplicits._
+
+  override def extract(answers: UserAnswers, data: Option[DisplayTrustWillType]): Either[PlaybackExtractionError, Try[UserAnswers]] =
+    {
+      data match {
+        case None => Right(Success(answers))
+        case deceasedSettlor =>
+
+          val updated = deceasedSettlor.foldLeft[Try[UserAnswers]](Success(answers)){
+            case (answers, deceasedSettlor) =>
+
+            answers
+              .flatMap(_.set(SettlorsNamePage, FullName(deceasedSettlor.name.firstName, deceasedSettlor.name.middleName, deceasedSettlor.name.lastName )))
+              .flatMap(answers => extractDateOfDeath(deceasedSettlor, answers))
+              .flatMap(answers => extractDateOfBirth(deceasedSettlor, answers))
+              .flatMap(answers => extractNino(deceasedSettlor, answers))
+              .flatMap(answers => extractAddress(deceasedSettlor, answers))
+              .flatMap(answers => extractPassportIdCard(deceasedSettlor, answers))
+              .flatMap(_.set(SettlorsSafeIdPage, deceasedSettlor.identification.flatMap(_.safeId)))
+              .flatMap {
+                _.set(
+                  SettlorsMetaData,
+                  MetaData(
+                    lineNo = deceasedSettlor.lineNo,
+                    bpMatchStatus = deceasedSettlor.bpMatchStatus,
+                    entityStart = deceasedSettlor.entityStart
+                  )
+                )
+              }
+          }
+
+          updated match {
+            case Success(a) =>
+              Right(Success(a))
+            case Failure(exception) =>
+              Logger.warn(s"[DeceasedSettlorExtractor] failed to extract data due to ${exception.getMessage}")
+              Left(FailedToExtractData(DisplayTrustWillType.toString))
+          }
+      }
+    }
+
+  private def extractDateOfDeath(deceasedSettlor: DisplayTrustWillType, answers: UserAnswers) = {
+    deceasedSettlor.dateOfDeath match {
+      case Some(dateOfDeath) =>
+        answers.set(SettlorDateOfDeathYesNoPage, true)
+          .flatMap(_.set(SettlorDateOfDeathPage, LocalDate.of(dateOfDeath.getYear, dateOfDeath.getMonthOfYear, dateOfDeath.getDayOfMonth)))
+      case None =>
+        // Assumption that user answered no as the date of death is not provided
+        answers.set(SettlorDateOfDeathYesNoPage, false)
+    }
+  }
+
+  private def extractDateOfBirth(deceasedSettlor: DisplayTrustWillType, answers: UserAnswers) = {
+    deceasedSettlor.dateOfBirth match {
+      case Some(dateOfBirth) =>
+        answers.set(SettlorDateOfBirthYesNoPage, true)
+          .flatMap(_.set(SettlorsDateOfBirthPage, LocalDate.of(dateOfBirth.getYear, dateOfBirth.getMonthOfYear, dateOfBirth.getDayOfMonth)))
+      case None =>
+        // Assumption that user answered no as the date of birth is not provided
+        answers.set(SettlorDateOfBirthYesNoPage, false)
+    }
+  }
+
+  private def extractNino(deceasedSettlor: DisplayTrustWillType, answers: UserAnswers) = {
+    deceasedSettlor.identification.flatMap(_.nino) match {
+      case Some(nino) =>
+        answers.set(SettlorNationalInsuranceNumberPage, nino)
+          .flatMap(_.set(SettlorsNINoYesNoPage, true))
+      case None =>
+        // Assumption that user answered no as nino is not provided
+        answers.set(SettlorsNINoYesNoPage, false)
+    }
+  }
+
+  private def extractAddress(deceasedSettlor: DisplayTrustWillType, answers: UserAnswers) = {
+    deceasedSettlor.identification.flatMap(_.address.convert) match {
+      case Some(uk: UKAddress) =>
+        answers.set(SettlorsUKAddressPage, uk)
+          .flatMap(_.set(SettlorsLastKnownAddressYesNoPage, true))
+          .flatMap(_.set(WasSettlorsAddressUKYesNoPage, true))
+      case Some(nonUk: InternationalAddress) =>
+        answers.set(SettlorsInternationalAddressPage, nonUk)
+          .flatMap(_.set(SettlorsLastKnownAddressYesNoPage, true))
+          .flatMap(_.set(WasSettlorsAddressUKYesNoPage, false))
+      case None => Try(answers)
+    }
+  }
+
+  private def extractPassportIdCard(deceasedSettlor: DisplayTrustWillType, answers: UserAnswers) = {
+    deceasedSettlor.identification.flatMap(_.passport) match {
+      case Some(passportIdCard) =>
+        answers.set(SettlorsPassportIDCardPage, PassportOrIdCardDetails(passportIdCard.countryOfIssue, passportIdCard.number, passportIdCard.expirationDate))
+      case None => Try(answers)
+    }
+  }
+
+
+}
---
+++ test/mapping/playback/DeceasedSettlorExtractorSpec.scala
@@ -0,0 +1,390 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mapping.playback
+
+import java.time.LocalDate
+
+import base.SpecBaseHelpers
+import generators.Generators
+import mapping.registration.PassportType
+import models.core.pages.{FullName, UKAddress}
+import models.playback.http._
+import models.playback.{MetaData, UserAnswers}
+import org.joda.time.DateTime
+import org.scalatest.{EitherValues, FreeSpec, MustMatchers}
+import pages.register.settlors.deceased_settlor._
+
+class DeceasedSettlorExtractorSpec extends FreeSpec with MustMatchers
+  with EitherValues with Generators with SpecBaseHelpers {
+
+  val deceasedSettlorExtractor : PlaybackExtractor[Option[DisplayTrustWillType]] =
+    injector.instanceOf[DeceasedSettlorExtractor]
+
+  "Deceased Settlor Extractor" - {
+
+    "when no Deceased Settlor" - {
+
+      "must return user answers" in {
+
+        val deceasedSettlor = None
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, deceasedSettlor)
+
+        extraction mustBe 'right
+
+      }
+
+    }
+
+    "when there is a Deceased Settlor" - {
+
+      "with minimum data of name, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = None,
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+
+      "with name and date of death, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = Some(DateTime.parse("2019-02-01")),
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = None,
+              passport = None,
+              address = None
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage).get mustBe LocalDate.of(2019,2,1)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name and date of birth, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = Some(DateTime.parse("1970-10-15")),
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = None,
+              passport = None,
+              address = None
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage).get mustBe LocalDate.of(1970,10,15)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name and nino, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = Some("NA1111111A"),
+              passport = None,
+              address = None
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage).get mustBe "NA1111111A"
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name and UK address, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = None,
+              passport = None,
+              address = Some(AddressType("line 1", "line2", None, None, Some("NE11NE"), "GB"))
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(WasSettlorsAddressUKYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) must be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage).get.postcode mustBe "NE11NE"
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name and International address, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = None,
+              passport = None,
+              address = Some(AddressType("Int line 1", "Int line2", None, None, None, "DE"))
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(WasSettlorsAddressUKYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) must be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage).get.country mustBe "DE"
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name and passport/ID Card, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = None,
+              passport = Some(PassportType("KSJDFKSDHF6456545147852369QWER", LocalDate.of(2020,2,2), "DE")),
+              address = None
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(WasSettlorsAddressUKYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) must be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage).get.country mustBe "DE"
+      }
+
+      "with name, date of death, date of birth and nino, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = Some(DateTime.parse("1970-10-15")),
+          dateOfDeath = Some(DateTime.parse("2019-02-01")),
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = None,
+              nino = Some("NA1111111A"),
+              passport = None,
+              address = None
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage).get mustBe LocalDate.of(2019,2,1)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage).get mustBe LocalDate.of(1970,10,15)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage).get mustBe "NA1111111A"
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) mustNot be(defined)
+      }
+
+      "with name, UK Address, passport/ID Card, metaData and safeId, must return user answers updated" in {
+
+        val deceasedSettlor = DisplayTrustWillType(
+          lineNo = "1",
+          bpMatchStatus = Some("01"),
+          name = NameType("First Name", None, "Last Name"),
+          dateOfBirth = None,
+          dateOfDeath = None,
+          identification = Some(
+            DisplayTrustIdentificationType(
+              safeId = Some("XK0000100152366"),
+              nino = None,
+              passport = Some(PassportType("KSJDFKSDHF6456545147852369QWER", LocalDate.of(2020,2,2), "DE")),
+              address = Some(AddressType("line 1", "line2", None, None, Some("NE11NE"), "GB"))
+            )
+          ),
+          entityStart = "2019-11-26"
+        )
+
+        val ua = UserAnswers("fakeId")
+
+        val extraction = deceasedSettlorExtractor.extract(ua, Some(deceasedSettlor))
+
+        extraction.right.value.success.value.get(SettlorsNamePage).get mustBe FullName("First Name", None, "Last Name")
+        extraction.right.value.success.value.get(SettlorDateOfDeathYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorDateOfDeathPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorDateOfBirthYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorsDateOfBirthPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsNINoYesNoPage).get mustBe false
+        extraction.right.value.success.value.get(SettlorNationalInsuranceNumberPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsLastKnownAddressYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(WasSettlorsAddressUKYesNoPage).get mustBe true
+        extraction.right.value.success.value.get(SettlorsUKAddressPage) must be(defined)
+        extraction.right.value.success.value.get(SettlorsInternationalAddressPage) mustNot be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage) must be(defined)
+        extraction.right.value.success.value.get(SettlorsPassportIDCardPage).get.country mustBe "DE"
+        extraction.right.value.success.value.get(SettlorsSafeIdPage).get mustBe "XK0000100152366"
+        extraction.right.value.success.value.get(SettlorsMetaData).get mustBe MetaData("1", Some("01"), "2019-11-26")
+
+      }
+
+    }
+
+  }
+
+}
