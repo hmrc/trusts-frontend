a33983df4b411521a30a93ac7bc995eb4caf5202
“JoshuaH01” <joshua.hill@digital.hmrc.gov.uk>
2020-02-03 17:40:34 +0000

    TRUS-496| Added trusteeOrgAddressYesNo

---
+++ app/controllers/register/trustees/TrusteeOrgAddressUkYesNoController.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.trustees
+
+import controllers.actions._
+import controllers.actions.register.{DraftIdRetrievalActionProvider, RegistrationDataRequiredAction, RegistrationIdentifierAction}
+import controllers.filters.IndexActionFilterProvider
+import forms.YesNoFormProvider
+import javax.inject.Inject
+import models.{Mode, NormalMode}
+import navigation.Navigator
+import pages.register.trustees.{IsThisLeadTrusteePage, TrusteeOrgAddressUkYesNoPage, TrusteeUtrYesNoPage}
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.RegistrationsRepository
+import sections.Trustees
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.register.trustees.TrusteeOrgAddressUkYesNoView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class TrusteeOrgAddressUkYesNoController @Inject()(
+                                              override val messagesApi: MessagesApi,
+                                              registrationsRepository: RegistrationsRepository,
+                                              navigator: Navigator,
+                                              validateIndex: IndexActionFilterProvider,
+                                              identify: RegistrationIdentifierAction,
+                                              getData: DraftIdRetrievalActionProvider,
+                                              requireData: RegistrationDataRequiredAction,
+                                              requiredAnswer: RequiredAnswerActionProvider,
+                                              formProvider: YesNoFormProvider,
+                                              val controllerComponents: MessagesControllerComponents,
+                                              view: TrusteeOrgAddressUkYesNoView
+                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private def actions(index: Int, draftId: String) =
+    identify andThen
+      getData(draftId) andThen
+      requireData andThen
+      validateIndex(index, Trustees) andThen
+      requiredAnswer(RequiredAnswer(IsThisLeadTrusteePage(index), routes.IsThisLeadTrusteeController.onPageLoad(NormalMode, index, draftId)))
+
+  def onPageLoad(mode: Mode, index: Int, draftId: String): Action[AnyContent] = actions(index, draftId) {
+    implicit request =>
+
+      val form: Form[Boolean] = formProvider.withPrefix("trusteeOrgAddressUkYesNo")
+
+      val preparedForm = request.userAnswers.get(TrusteeOrgAddressUkYesNoPage(index)) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, mode, draftId, index))
+  }
+
+  def onSubmit(mode: Mode, index: Int, draftId: String): Action[AnyContent] = actions(index, draftId).async {
+    implicit request =>
+
+      val form: Form[Boolean] = formProvider.withPrefix("trusteeOrgAddressUkYesNo")
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(view(formWithErrors, mode, draftId, index))),
+
+        value => {
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(TrusteeOrgAddressUkYesNoPage(index), value))
+            _              <- registrationsRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(TrusteeUtrYesNoPage(index), mode, draftId)(updatedAnswers))
+        }
+      )
+  }
+}
