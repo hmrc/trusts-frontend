c3a9482f8a13bd5b897e8372e222ee435fe6dea2
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-03-17 13:06:01 +0000

    TRUS-3901| Removed playback logger

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.8-play27",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.60.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.4.0-play-26",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "3.2.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "3.4.0",
     "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "org.typelevel"     %% "cats-core"                      % "2.0.0",
