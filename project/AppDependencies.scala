a4a29df32db9a9b73c8eafae93750f928425ac55
Adam Conder <adamdconder@gmail.com>
2021-10-11 09:32:15 +0100

    Fix integration tests and pegdown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.20.13-play28",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "1.1.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.9.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.9.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.9.0",
     "uk.gov.hmrc"       %% "domain"                         % "6.2.0-play-28",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
@@ -19,7 +19,6 @@ object AppDependencies {
     "org.pegdown"              % "pegdown"                % "1.6.0",
     "org.scalatest"           %% "scalatest"              % "3.0.9",
     "org.scalatestplus.play"  %% "scalatestplus-play"     % "5.0.0",
-    "uk.gov.hmrc"             %% "hmrctest"               % "3.9.0-play-26",
     "org.jsoup"                % "jsoup"                  % "1.12.1",
     "com.typesafe.play"       %% "play-test"              % PlayVersion.current,
     "org.mockito"              % "mockito-all"            % "1.10.19",
@@ -29,8 +28,9 @@ object AppDependencies {
   ).map(_ % "test")
 
   val it: Seq[ModuleID] = Seq(
-    "org.scalatestplus.play"  %% "scalatestplus-play"     % "4.0.3",
-    "uk.gov.hmrc"             %% "hmrctest"               % "3.9.0-play-26"
+    "org.scalatest"           %% "scalatest"              % "3.0.9",
+    "org.scalatestplus.play"  %% "scalatestplus-play"     % "5.0.0",
+    "org.pegdown"              % "pegdown"                % "1.6.0"
   ).map(_ % "it")
 
   def apply(): Seq[ModuleID] = compile ++ test ++ it
