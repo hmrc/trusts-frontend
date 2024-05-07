14a83751a4551ab0ecb44073e9b4ce9c220cd5be
Victor Osipov <osipovvictor1994@gmail.com>
2024-05-07 17:17:13 +0100

    DDCE 5199 - Play 3.0 Upgrade (#879)

--- it/test/repositories/CacheRepositorySpec.scala
+++ it/test/repositories/CacheRepositorySpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,9 @@ import org.scalatest.{BeforeAndAfterEach, OptionValues}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.wordspec.AnyWordSpec
+import repositories.CacheRepositoryImpl
 import uk.gov.hmrc.mongo.test.MongoSupport
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,31 +2,28 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.23.0"
-  val mongoVersion = "1.7.0"
+  private val bootstrapVersion = "8.5.0"
+  private val mongoVersion = "1.9.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"     % "8.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28",
-    "uk.gov.hmrc"       %% "tax-year"                       % "4.0.0",
-    "org.typelevel"     %% "cats-core"                      % "2.10.0"
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "8.5.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                               % "4.0.0",
+    "org.typelevel"     %% "cats-core"                              % "2.10.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatest"           %% "scalatest"                % "3.2.18",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % mongoVersion,
     "org.jsoup"               %  "jsoup"                    % "1.17.2",
-    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.30",
-    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.17.0",
-    "io.github.wolfendale"    %% "scalacheck-gen-regexp"    % "1.1.0",
-    "org.wiremock"            % "wiremock-standalone"      % "3.4.2",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8"
-  ).map(_ % "test, it")
+    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.31",
+    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0",
+    "io.github.wolfendale"    %% "scalacheck-gen-regexp"    % "1.1.0"
+  ).map(_ % Test)
 
 
   def apply(): Seq[ModuleID] = compile ++ test
