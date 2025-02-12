6626000d2d2d05a858b79124f76ec60aa5b17578
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-02-12 11:00:54 +0000

    DDCE-3719 - updating the bas-gateway req parameters

--- app/controllers/actions/TrustsAuthorisedFunctions.scala
+++ app/controllers/actions/TrustsAuthorisedFunctions.scala
@@ -34,7 +34,7 @@ class TrustsAuthorisedFunctions @Inject()(override val authConnector: AuthConnec
 
   def redirectToLogin: Result = {
     logger.debug("Redirecting to Login")
-    Redirect(config.loginUrl, Map("continue" -> Seq(config.loginContinueUrl),
+    Redirect(config.loginUrl, Map("continue_url" -> Seq(config.loginContinueUrl),
       "origin" -> Seq(config.appName), "accountType" -> Seq(config.affinityGroup)
     ))
   }
