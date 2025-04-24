bc048cc73889f00e898ed12a99fb0b3ac05c8275
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-04-24 13:26:17 +0100

    DDCE-5308 PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.1.0"
-  private val mongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.11.0"
+  private val mongoVersion = "2.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "9.11.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.1.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
     "uk.gov.hmrc"       %% "tax-year"                               % "5.0.0",
     "org.typelevel"     %% "cats-core"                              % "2.12.0"
