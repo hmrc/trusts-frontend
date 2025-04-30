86e853e6c49c222d5eb21110abc02109ded50219
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-04-30 08:08:39 +0100

    DDCE-6346 fix scriptsBlock in MainTemplate

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,17 +9,17 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "9.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
-    "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
-    "uk.gov.hmrc"       %% "tax-year"                               % "5.0.0",
-    "org.typelevel"     %% "cats-core"                              % "2.12.0"
+    "uk.gov.hmrc"       %% "domain-play-30"                         % "11.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                               % "6.0.0",
+    "org.typelevel"     %% "cats-core"                              % "2.13.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % mongoVersion,
-    "org.jsoup"               %  "jsoup"                    % "1.18.1",
+    "org.jsoup"               %  "jsoup"                    % "1.20.1",
     "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.37",
     "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0",
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"    % "1.1.0"
