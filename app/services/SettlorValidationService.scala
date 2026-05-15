f8a11af841797067c9bb65e004a76d1fcda976d7
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-05-15 12:16:12 +0100

    DDCE-8535 add back check for living & deceased settlor, might help w future spike

--- app/services/SettlorValidationService.scala
+++ app/services/SettlorValidationService.scala
@@ -39,13 +39,11 @@ class SettlorValidationService @Inject() {
             val hasOtherSettlors =
               individualSettlors.exists(_.value.nonEmpty) || companySettlors.exists(_.value.nonEmpty)
 
-            // if (hasOtherSettlors) {
-            // List(s"$registrationPrefix: deceased settlor cannot coexist with other settlors") // todo: remove this is fine, but the UI doesn't allow it?
-            // } else {
-            // validateDeceasedSettlor(deceasedData, registrationPrefix)
-            // }
-
-            validateDeceasedSettlor(deceasedData, registrationPrefix)
+            if (hasOtherSettlors) {
+              List(s"$registrationPrefix: deceased settlor cannot coexist with other settlors")
+            } else {
+              validateDeceasedSettlor(deceasedData, registrationPrefix)
+            }
 
           case None =>
             val hasIndividualSettlors = individualSettlors.exists(_.value.nonEmpty)
