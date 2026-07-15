46d0e783f0a81b18f3bfc2eafde32b4fb9beb1db
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2026-07-15 15:01:07 +0100

    DDCE-8580 Upgrade play-frontend-hmrc in trusts-frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.scalatestplus"    %% "scalacheck-1-19"         % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"         % "3.2.20.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
